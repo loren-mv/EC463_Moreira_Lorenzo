3669a57ffa337e0fe7e09ca192b44c18ebf6a660
Lorenzo Moreira <ljmv@bu.edu>
2025-10-17 18:43:31 -0400

    Creation of Simulation Files and Conversion Logic

--- Posit/Posit.srcs/sources_1/new/convert.v
+++ Posit/Posit.srcs/sources_1/new/convert.v
@@ -20,27 +20,56 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module convert #(parameter N = 8)(
-    input isZero,
-    input isNaR,
-    input isLessThanOne,
-    output reg [N-1:0] converted_num
+module convert #(parameter N = 32, parameter ES = 2)(
+    input clk,        
+    input rst,        
+    input[31:0]  fp32_in,    // IEEE 754 float input
+    output reg [N-1:0] posit_out   
     );
+
+    wire sign;
+    wire [7:0] exp;
+    wire [22:0] frac;
+
+    assign sign = fp32_in[31];
+    assign exp  = fp32_in[30:23];
+    assign frac = fp32_in[22:0];
     
-    always@(*) begin
-        
-        if(isZero) begin
-            converted_num = {N{1'b0}};
-        end
-        
-        else if(isNaR) begin
-            converted_num = {N{1'b1}};
-        end
-        
-        else begin
-        
-        end
+    wire signed [8:0] E = exp - 127;      
+    wire [23:0] M = {1'b1, frac};
     
+    wire signed [8:0] k = E >>> ES;       
+    wire [ES-1:0] exp_posit = E[ES-1:0];
+    
+    reg [N-2:0] regime_bits;
+    integer regime_size;
+    reg [N-1:0] posit_comb;
+    
+      always @(*) begin
+        regime_bits = 'b0;
+        regime_size = 0;
+        posit_comb  = 'b0;
+
+        if (k >= 0) begin
+            regime_size = k + 2;  // run of 1's + terminating 0
+            regime_bits = { (N-1){1'b1} };
+            regime_bits[N - regime_size] = 1'b0;
+        end else begin
+            regime_size = (-k) + 1; // run of 0's + terminating 1
+            regime_bits = { (N-1){1'b0} };
+            regime_bits[N - regime_size] = 1'b1;
+        end
+
+        posit_comb = {sign, regime_bits[N-2:0]};
+
+        // (Simplified: not including fraction rounding/truncation logic)
+    end
+    
+    always @(posedge clk) begin
+        if (rst)
+            posit_out <= {N{1'b0}};
+        else
+            posit_out <= posit_comb;
     end
     
 endmodule
