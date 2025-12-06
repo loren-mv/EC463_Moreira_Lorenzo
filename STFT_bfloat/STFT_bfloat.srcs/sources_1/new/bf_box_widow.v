49055cd5659ac04ec73310c3394b4af10b414d8d
Lorenzo Moreira <ljmv@bu.edu>
2025-12-06 13:27:50 -0500

    Bfloat_Design Files

---
+++ STFT_bfloat/STFT_bfloat.srcs/sources_1/new/bf_box_widow.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12/06/2025 01:22:45 PM
+// Design Name: 
+// Module Name: bf_box_widow
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module bf_box_widow(
+
+    );
+endmodule
---
+++ STFT_bfloat/STFT_bfloat.srcs/sources_1/new/complex_bfmult.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12/06/2025 01:18:50 PM
+// Design Name: 
+// Module Name: complex_bfmult
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module complex_bfmult(
+
+    );
+endmodule
